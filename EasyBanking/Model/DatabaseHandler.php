17ffac4011a6a468dc2a7cc68f47bca55367b4e1
Jonas Sticha <jonas.sticha@tum.de>
2015-10-24 11:41:47 +0200

    Model/DatabaseHandler: Preliminary implementation of connect and execQuery methods

--- EasyBanking/Model/DatabaseHandler.php
+++ EasyBanking/Model/DatabaseHandler.php
@@ -2,6 +2,9 @@
 class DatabaseHandler {
     static private $instance = null;
 
+    private $mysqlConnection;
+    private $connected = FALSE;
+
     static public function getInstance() {
         if (null === self::$instance) {
             self::$instance = new self;
@@ -10,5 +13,25 @@ class DatabaseHandler {
     }
 
     private function __construct(){}
+
+    private function connectToDB() {
+        $connected = FALSE;
+        //TODO: Enter correct connection information below
+        $mysqlConnection = new mysqli("127.0.0.1", "user", "password", "database", 3306);
+        if($mysqlConnection->connect_errno) {
+            echo "Couldn't connect to DB: " . $mysqlConnection->connect_error;
+            return FALSE;
+        }
+        $connected = TRUE;
+        return TRUE;
+    }
+
+    public function execQuery($query) {
+        if($connected == FALSE && connectToDB() == FALSE) {
+            return;
+        }
+        $result = $mysqlConnection->query($query);
+        return $result;
+    }
 }
 ?>
