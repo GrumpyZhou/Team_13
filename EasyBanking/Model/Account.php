719b1b90aa737bfc199be8978930351afd4316bd
Jonas Sticha <jonas.sticha@tum.de>
2015-10-24 11:41:05 +0200

    Model/Account: Partially implement login method

--- EasyBanking/Model/Account.php
+++ EasyBanking/Model/Account.php
@@ -1,11 +1,19 @@
 <?php
+include_once "DatabaseHandler.php";
 abstract class Account {
     private $firstName;
     private $lastName;
     private $email;
 
-    abstract public function login($email, $password);
-    abstract public function logout();
+    public function login($userID, $password) {
+        $dbHandler = DatabaseHandler::getInstance();
+        $res = $dbHandler->execQuery("SELECT * FROM users WHERE id='" . $userID . "';");
+        $row = $res->fetch_assoc();
+        $storedPW = $row['password'];
+        //TODO: Compare Password hashes, if true return object of Employee or Customer
+    }
+
+    abstract public function logout(); //TODO
 
     public function register($email, $name, $password, $isEmployee) {
         return "ERROR: Not implemented yet";
