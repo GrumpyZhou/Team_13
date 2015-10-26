b4fd97afdbb32638decff8f61e83abca6c33138c
Qunjie Zhou <jennyzqj00@gmail.com>
2015-10-26 20:13:16 +0100

    Add 2 controllers

---
+++ EasyBanking/Controller/LoginCtrl.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+require_once('../Model/Account.php');
+
+if (!isset($_SESSION['authenticated'])) {
+    $_SESSION['isAuthenticated'] = false;   // to mark whether the  user  has been authenticated
+}
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+
+    while (false) { // the block is just for test
+        $user = login($email, $password);    // Call interface to authenticate
+        if (isset($user)) {
+            $_SESSION['currentUser'] = $user;
+            if ($user instanceof Customer) {
+                $_SESSION['isEmployee'] = false;
+                header("Location:../View/account.html");
+            } else {
+                $_SESSION['isEmployee'] = false;
+                header("Location:../View/administration.html");
+            }
+        }
+    }
+
+  //testing ...
+  header("Location:../View/account.html");
+}
+
+
+
---
+++ EasyBanking/Controller/RegisterCtrl.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('../Model/Account.php');
+
+
+$email = $_POST['email'];
+$firstName = $_POST['fname'];
+$lastName = $_POST['lname'];
+$password = $_POST['password'];
+$isEmployee = isset($_POST['yes'])? true:false;
+
+
+echo $email." ".$firstName." ".$lastName." ".$password." ".$isEmployee; //testing
+
+// call function:  don't know how to call it now
+// register($email,$firstName,$lastName,$password,$isEmployee);
+
+
