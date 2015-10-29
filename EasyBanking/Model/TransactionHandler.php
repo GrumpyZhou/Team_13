81feb2c15ea7f7428a3d432d5548af2a7f9b138f
Falambo <danielsuttor@gmail.com>
2015-10-29 19:28:07 +0100

    Model/TransactionHandler: GetTransactionHistory implemented

--- EasyBanking/Model/TransactionHandler.php
+++ EasyBanking/Model/TransactionHandler.php
@@ -1,6 +1,22 @@
 <?php
+include_once "DatabaseHandler.php";
+
 class TransactionHistory {
     function __construct(){
+    }
+
+    public static function GetTransactionHistory($userId)
+    {
+        $dbHandler = DatabaseHandler::getInstance();
+        $history = $dbHandler->execQuery("SELECT * FROM transactions WHERE sender_id='" . $userId . "';");
+        while($row = $history->fetch_assoc())
+        {
+            echo "<tr>";
+            echo "<td>" . $row['transaction_date'] . "</td>";
+            echo "<td>" . $row['receiver_id'] . "</td>";
+            echo "<td>" . $row['amount'] . "</td>";
+            echo "</tr>";
+        }
     }
 }
 ?>
