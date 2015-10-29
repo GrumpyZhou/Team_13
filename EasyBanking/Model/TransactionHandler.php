6ff0f0650a54467710aa0692f707b76af1fdfc74
Falambo <danielsuttor@gmail.com>
2015-10-29 21:30:18 +0100

    Model/TransactionHandler: return transaction data array

--- EasyBanking/Model/TransactionHandler.php
+++ EasyBanking/Model/TransactionHandler.php
@@ -1,6 +1,19 @@
 <?php
 include_once "DatabaseHandler.php";
 
+class Transaction
+{
+    public $date;
+    public $receiverIBAN;
+    public $amount;
+    function __construct($date, $IBAN, $amount)
+    {
+        $this->date = $date;
+        $this->receiverIBAN = $IBAN;
+        $this->amount = $amount;
+    }
+}
+
 class TransactionHistory {
     function __construct(){
     }
@@ -9,14 +22,14 @@ class TransactionHistory {
     {
         $dbHandler = DatabaseHandler::getInstance();
         $history = $dbHandler->execQuery("SELECT * FROM transactions WHERE sender_id='" . $userId . "';");
+
+        $dataArray = array();
         while($row = $history->fetch_assoc())
         {
-            echo "<tr>";
-            echo "<td>" . $row['transaction_date'] . "</td>";
-            echo "<td>" . $row['receiver_id'] . "</td>";
-            echo "<td>" . $row['amount'] . "</td>";
-            echo "</tr>";
+            $dataArray[] = new Transaction($row['transaction_date'], $row['receiver_id'], $row['amount']);
         }
+
+        return dataArray;
     }
 }
 ?>
