795b811f5bfd68332dafad7b671083a53e544dac
Samurai <samurai@samurai-wtf.localhost>
2015-10-31 23:37:51 +0100

    add link to  return to the homepage

--- EasyBanking/Controller/RegisterCtrl.php
+++ EasyBanking/Controller/RegisterCtrl.php
@@ -1,20 +1,20 @@
 <?php
 
 require_once('../Model/Account.php');
-
-
 $email = $_POST['email'];
 $firstName = $_POST['fname'];
 $lastName = $_POST['lname'];
 $password = $_POST['password'];
 $isEmployee = isset($_POST['yes'])? true:false;
 
+$result=Account::register($email,$firstName,$lastName,$password,$isEmployee);
+if(is_string($result)){
+	
+	echo "Error:".$result;}
+	else{
+		echo "You have registered successfully!";
+		echo "<a href='../View/index.php'>Click here to go back to the HomePage<//a>";
+}
 
-echo $email." ".$firstName." ".$lastName." ".$password." ".$isEmployee; //testing
-
-// call function:  don't know how to call it now
-// register($email,$firstName,$lastName,$password,$isEmployee);
 
 
-//return the front page
-header("Location:../View/index.php");
