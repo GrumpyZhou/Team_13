198f746155b84a18d34a88e95e56b19a7c669d97
Falambo <danielsuttor@gmail.com>
2015-10-31 19:22:40 +0100

    Model/TransactionHandler: implemented ExportPDF
    fpdf added to 3d party folder

--- EasyBanking/Model/TransactionHandler.php
+++ EasyBanking/Model/TransactionHandler.php
@@ -1,20 +1,34 @@
 <?php
 include_once "DatabaseHandler.php";
+require('3rd Party\fpdf.php');
 
 class Transaction
 {
     public $date;
     public $receiverIBAN;
     public $amount;
+
+    private static $cellWidth = 60;
+    private static $cellHeight = 8;
+
     function __construct($date, $IBAN, $amount)
     {
         $this->date = $date;
         $this->receiverIBAN = $IBAN;
         $this->amount = $amount;
     }
+
+    public static function WriteData($date, $iban, $amount, $pdf)
+    {
+       $pdf->Cell(self::$cellWidth,self::$cellHeight, $date,0, 0);
+       $pdf->Cell(self::$cellWidth,self::$cellHeight, $iban,0,0);
+       $pdf->Cell(self::$cellWidth,self::$cellHeight, $amount, 0,1);
+    }
 }
 
 class TransactionHistory {
+    private static $fileName = "../Upload/TransactionHistory_";
+
     function __construct(){
     }
 
@@ -30,6 +44,26 @@ class TransactionHistory {
         }
 
         return dataArray;
+    }
+
+    //Returns the path to the created pdf file
+    public static function ExportPDF($userId)
+    {
+        $pdf = new FPDF();
+        $pdf->AddPage();
+        $pdf->SetFont('Arial', 'B', 16);
+
+        Transaction::WriteData("Transaction Date", "IBAN", "Amount", $pdf);
+        $dataArray = self::GetTransactionHistory($userId);
+        foreach($dataArray as &$element)
+        {
+            Transaction::WriteData($element->date, $element->receiverIBAN, $element->amount, $pdf);
+        }
+
+        $file = self::$fileName . $userId . ".pdf";
+        $pdf->Output($file);
+
+        return $file;
     }
 }
 ?>
