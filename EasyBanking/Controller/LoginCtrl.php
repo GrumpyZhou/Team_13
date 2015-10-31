cc1f6c31dea9f34cfcce6db98f7e84a4916f044d
Samurai <samurai@samurai-wtf.localhost>
2015-10-31 20:17:53 +0100

    use the interfaces provided by Model

--- EasyBanking/Controller/LoginCtrl.php
+++ EasyBanking/Controller/LoginCtrl.php
@@ -5,24 +5,27 @@ require_once('../Model/Account.php');
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
-
-
-    while (false) { //the block ck is just for test
-        $user = login($email, $password);    // Call interface to authenticate
-        if (isset($user)) {
-            $_SESSION['currentUser'] = $user;
+    $user =Account::login($email, $password);  
+        if(is_string($user)){
+			echo "Error message: ".$user;
+			
+			}
+        else{
+            $_SESSION['email'] = $user->email;
+            $_SESSION['firstname'] = $user->firstName;
+            $_SESSION['lastname'] = $user->lastName;
+       
             if ($user instanceof Customer) {
                 $_SESSION['isEmployee'] = false;
+                $_SESSION['iban'] = $user->IBAN;
+               // $_SESSION['balance'] = $user->balance;   
                 header("Location:../View/account.php");
             } else {
-                $_SESSION['isEmployee'] = false;
+                $_SESSION['isEmployee'] = true;
                 header("Location:../View/administration.php");
             }
         }
     }
-    header("Location:../View/account.php"); //also for test
-
-}
-
+//header("Location:../View/account.php"); //also for test
 
 
