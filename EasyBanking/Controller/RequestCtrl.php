6dee2021f4cffd2c37c81d567195dbab81abf5bf
Falambo <danielsuttor@gmail.com>
2015-11-01 22:39:36 +0100

    Controller/RequestCtrl: redirect to administration page

--- EasyBanking/Controller/RequestCtrl.php
+++ EasyBanking/Controller/RequestCtrl.php
@@ -5,27 +5,23 @@ if (isset($_POST['reqtype'])) {
     $reqtype = $_POST['reqtype'];
   //echo $reqtype;
     if ($reqtype == 'registration') {
-        $id=$_POST['id']; 
+        $id=$_POST['id'];
         $action=$_POST['action'];
         if($action=='Accept'){
-			 // $transaction: Boolean
-			RequestHandler::approveRequest($id, false);
-		echo  "approve registration of  ".$id;
-		}else{
-			RequestHandler::denyRequest($id,false);
-		}
+             // $transaction: Boolean
+            RequestHandler::approveRequest($id, false);
+        }else{
+            RequestHandler::denyRequest($id,false);
+        }
     } elseif ($reqtype == 'transaction') {
-        $id=$_POST['id'];  
+        $id=$_POST['id'];
         $action=$_POST['action'];
         if($action=='Accept'){
-			 // $transaction: Boolean
-			RequestHandler::approveRequest($id, true);
-		echo  "approve transaction of  ".$id;
-		}else{
-			RequestHandler::denyRequest($id,true);
-		}
+             // $transaction: Boolean
+            RequestHandler::approveRequest($id, true);
+        }else{
+            RequestHandler::denyRequest($id,true);
+        }
     }
 }
-
-//return to the administration page
 header("Location:../View/administration.php");
