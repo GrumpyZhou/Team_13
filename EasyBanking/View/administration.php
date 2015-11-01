15313684ec65c417dd05c09a96da8f8874e53a88
Falambo <danielsuttor@gmail.com>
2015-11-01 22:50:58 +0100

    View/administration: removed placeholder values

--- EasyBanking/View/administration.php
+++ EasyBanking/View/administration.php
@@ -5,7 +5,7 @@ require_once('../Model/RequestHandler.php');
 if (!isset($_SESSION['isEmployee'])||!$_SESSION['isEmployee']) {
    header("Location:../View/index.php");
 } else {
-	//echo "admin page";
+    //echo "admin page";
 //can not get these fields !!
 $firstname = $_SESSION['firstname'];
 $lastname = $_SESSION['lastname'];
@@ -40,7 +40,7 @@ echo $firstname;
         </div>
     </div>
 
-    <div class="mainpart">
+<div class="mainpart">
 
         <div class="banner">
             <h1>Requests To Be Handled!</h1>
@@ -59,35 +59,23 @@ echo $firstname;
                       //true: get registration requests
                       $registerRequest=RequestHandler::getOpenRequests(true);
                       foreach($registerRequest as $request){
-						  $email=$request->mail;
-						  $date=$request->date;
-						  $id=$request->id;
+                          $email=$request->mail;
+                          $date=$request->date;
+                          $id=$request->id;
                         ?>
                         <tr>
                             <td><?php echo $date;?></td>
                             <td><?php echo $email;?></td>
                             <td>
-								<form action="../Controller/RequestCtrl.php" method="post">
-                                   <input type="hidden" name="reqtype" value="registration"/>
-                                   <input type="hidden" name="id" value="<?php echo $id;?>"/>
-								   <input class="barbtn" name="action" type="submit" value="Deny"/>
-                                   <input class="barbtn" name="action" type="submit" value="Accept"/>								   <input type="hidden" name="email" value="<?php $email;?>" />
-								</form> 
-							</td> 
-						</tr>
-                        <?php } ?>
-                        <tr>
-                            <td>20.01.2015</td>
-                            <td>222@gmail.com</td>
-                            <td>
-								<form action="../Controller/RequestCtrl.php" method="post">
+                                <form action="../Controller/RequestCtrl.php" method="post">
                                    <input type="hidden" name="reqtype" value="registration"/>
                                    <input type="hidden" name="id" value="<?php echo $id;?>"/>
                                    <input class="barbtn" name="action" type="submit" value="Deny"/>
-                                   <input class="barbtn" name="action" type="submit" value="Accept"/>								   <input type="hidden" name="email" value="<?php $email;?>" />
-								</form> 
-							</td> 
+                                   <input class="barbtn" name="action" type="submit" value="Accept"/>                                  <input type="hidden" name="email" value="<?php $email;?>" />
+                                </form>
+                            </td>
                         </tr>
+                        <?php } ?>
                 </table>
             </div>
         </div>
@@ -104,48 +92,33 @@ echo $firstname;
                             <th>Amount</th>
                             <th>Approval</th>
                         </tr>
-                        
+
                         <?php
                         // here get the transaction request list to be handled and iterate and show in the table
                         //false: get transaction requests
                       $registerRequest=RequestHandler::getOpenRequests(false);
                       foreach($registerRequest as $request){
-						  $sender=$request->senderId;
-						  $date=$request->date;
-						  $amount=$request->amount;
-						  $id=$request->transactionId;
-                        ?>
-                   
+                          $sender=$request->senderId;
+                          $date=$request->date;
+                          $amount=$request->amount;
+                          $id=$request->transactionId;
+?>
+
                         <tr>
                             <td><?php echo $date;?></td>
                             <td><?php echo $sender;?></td>
                             <td><?php echo $amount;?></td>
                             <td>
-								<form action="../Controller/RequestCtrl.php" method="post">
-								   <input type="hidden" name="reqtype" value="transaction"/>
-								   <input type="hidden" name="id" value="<?php echo $id;?>"/>
-								   <input class="barbtn" name="action" type="submit" value="Deny"/>
+                                <form action="../Controller/RequestCtrl.php" method="post">
+                                   <input type="hidden" name="reqtype" value="transaction"/>
+                                   <input type="hidden" name="id" value="<?php echo $id;?>"/>
+                                   <input class="barbtn" name="action" type="submit" value="Deny"/>
                                    <input class="barbtn" name="action" type="submit" value="Accept"/>
-								   <input type="hidden" name="sender" value="<?php $sender;?>" />
-								</form> 
-							</td> 
-						</tr>
+                                   <input type="hidden" name="sender" value="<?php $sender;?>" />
+                                </form>
+                            </td>
+                        </tr>
                         <?php } ?>
-            
-                        <tr>
-                            <td>20.01.2015</td>
-                            <td>xxxx xxxx</td>
-                            <td>+xxx.xxx</td>
-                            <td>
-								<form action="../Controller/RequestCtrl.php" method="post">
-								   <input type="hidden" name="reqtype" value="transaction"/> 
-								   <input type="hidden" name="id" value="<?php echo $id; ?>"/>
-								   <input class="barbtn" name="action" type="submit" value="Deny"/>
-                                   <input class="barbtn" name="action" type="submit" value="Accept"/>
-								   <input type="hidden" name="sender" value="<?php $sender;?>" />
-								</form> 
-							</td>
-                        </tr> 
                 </table>
             </div>
         </div>
