381d8d8acbe3b1cf50068b162eae6a568c2aa406
Jonas Sticha <jonas.sticha@tum.de>
2015-11-02 00:53:09 +0100

    Controller/TransactionCtrl: Implement single transaction processing

--- EasyBanking/Controller/TransactionCtrl.php
+++ EasyBanking/Controller/TransactionCtrl.php
@@ -3,22 +3,22 @@ session_start();
 require_once('../Model/MoneyTransferHandler.php');
 
 if (isset($_POST['type'])) {
+    $id = $_SESSION['iban'];
+    $uploadFilePath = $_SERVER['DOCUMENT_ROOT'] . "/Upload/TransactionBatch_" . $id . ".txt";
     $type = $_POST['type'];
     if ($type == 'single') {
         $iban = $_POST['iban'];
         $amount = $_POST['amount'];
         $tid = $_POST['tid'];
         $tan = $_POST['tan'];
-        // .... call function for dealing and any returned message?
-        //MoneyTransferHandler::transferMoney($SESSION_['iban'], $iban, $amount, $tan, $tid);
-        
-       // echo $iban." ".$amount." ".$tid." ".$tan;
-
+        $rc = MoneyTransferHandler::transferMoney($id, $iban, $amount, $tan, $tid, $uploadFilePath);
+        if($rc != 0) {
+            echo "ERROR: Transfer could not be processed! Error Code: $rc";
+            return;
+        }
     } elseif ($type == 'multiple') {
         $tid = $_POST['tid'];
         $tan = $_POST['tan'];
-        $id = $_SESSION['iban'];
-        $uploadFilePath = $_SERVER['DOCUMENT_ROOT'] . "/Upload/TransactionBatch_" . $id . ".txt";
         if(move_uploaded_file($_FILES['batchfile']['tmp_name'], $uploadFilePath)) {
             $rc = MoneyTransferHandler::parseBatchFile($senderID, $uploadFilePath, $tid, $tan);
             if($rc != 0) {
