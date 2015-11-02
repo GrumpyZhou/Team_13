f5f82de79b786929b015807da0d30b8751a74a7b
Jonas Sticha <jonas.sticha@tum.de>
2015-11-02 01:32:29 +0100

    Controller/DownloadHistory: Add accessibility for employee

--- EasyBanking/Controller/DownloadHistory.php
+++ EasyBanking/Controller/DownloadHistory.php
@@ -2,7 +2,11 @@
 session_start();
 require_once('../Model/TransactionHistory.php');
 
-$id = $_SESSION['iban'];
+if(isset($_POST['iban'])) {
+    $id = $_POST['iban'];
+} else {
+    $id = $_SESSION['iban'];
+}
 $outputPathAbs = "/Upload/TransactionHistory_" . $id . ".pdf";
 $outputPathRel = ".." . $outputPathAbs;
 $outputPathAbs = $_SERVER['DOCUMENT_ROOT'] . $outputPathAbs;
