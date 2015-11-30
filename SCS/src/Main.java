d1713401b12a8641de6044a7896b44cf5f905b25
Jonas Sticha <jonas.sticha@tum.de>
2015-11-30 22:53:14 +0100

    SCS: Add initial Main class

---
+++ SCS/src/Main.java
@@ -0,0 +1,9 @@
+import gui.MainFrame;
+
+public class Main {
+    public static void main(String args[]) {
+        MainFrame gui = new MainFrame();
+        gui.makeVisible();
+        //TODO
+    }
+}
