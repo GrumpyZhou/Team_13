5403b7178eb0321a3277afe781ba2201c8503e5a
Jonas Sticha <jonas.sticha@tum.de>
2015-12-02 13:42:17 +0100

    SCS: Implement input sanity checker

--- SCS/src/gui/InputSanityChecker.java
+++ SCS/src/gui/InputSanityChecker.java
@@ -5,17 +5,34 @@ public class InputSanityChecker {
     public InputSanityChecker() {}
 
     public boolean checkPinInput(String input) {
-        //TODO
-        return false;
+        if(input.length() != 6)
+            return false;
+        for(char c : input.toCharArray()) {
+            if(c < '0' || c > '9')
+                return false;
+        }
+        return true;
     }
 
     public boolean checkTargetAccountInput(String input) {
-        //TODO
-        return false;
+        if(input.length() > 10)
+            return false;
+        for(char c : input.toCharArray()) {
+            if(c < '0' || c > '9')
+                return false;
+        }
+        return true;
     }
 
     public boolean checkTransferAmountInput(String input) {
-        //TODO
-        return false;
+        double amount;
+        try {
+            amount = Double.parseDouble(input);
+            if(amount <= 0)
+                return false;
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
