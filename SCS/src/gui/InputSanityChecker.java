54d902af68660c6a6532ce1a33fc38cec830491c
Jonas Sticha <jonas.sticha@tum.de>
2015-12-02 12:56:22 +0100

    SCS: Add initial input sanity checker

---
+++ SCS/src/gui/InputSanityChecker.java
@@ -0,0 +1,21 @@
+package gui;
+
+public class InputSanityChecker {
+
+    public InputSanityChecker() {}
+
+    public boolean checkPinInput(String input) {
+        //TODO
+        return false;
+    }
+
+    public boolean checkTargetAccountInput(String input) {
+        //TODO
+        return false;
+    }
+
+    public boolean checkTransferAmountInput(String input) {
+        //TODO
+        return false;
+    }
+}
