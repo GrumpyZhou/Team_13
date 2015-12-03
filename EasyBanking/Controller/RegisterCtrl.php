8a621d4f828295b30536b27d8537bf3efdc7c64a
Qunjie Zhou <jennyzqj00@gmail.com>
2015-12-03 10:52:55 +0100

    Check email on serverside; Clean data before store into DB

--- EasyBanking/Controller/RegisterCtrl.php
+++ EasyBanking/Controller/RegisterCtrl.php
@@ -1,20 +1,27 @@
 <?php
 
 require_once('../Model/Account.php');
-$email = $_POST['email'];
-$firstName = $_POST['fname'];
-$lastName = $_POST['lname'];
-$password = $_POST['password'];
-$isEmployee = isset($_POST['yes'])? true:false;
 
-$result=Account::register($email,$firstName,$lastName,$password,$isEmployee);
-if(is_string($result)){
-	
-	echo "Error:".$result;}
-	else{
-		echo "You have registered successfully!";
-		echo "<a href='../View/index.php'>Click here to go back to the HomePage<//a>";
+//check whether the email format is correct. It is also done here in case client side fails to check it.
+if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+
+    //remove html special characters before store the data.
+    $firstName = htmlspecialchars($_POST['fname']);
+    $lastName = htmlspecialchars($_POST['lname']);
+    $password = $_POST['password'];
+    $isEmployee = isset($_POST['yes']) ? true : false;
+
+
+    $result = Account::register($email, $firstName, $lastName, $password, $isEmployee);
+    if (is_string($result)) {
+
+        echo "Error:" . $result;
+    } else {
+        echo "You have registered successfully!";
+        echo "<a href='../View/index.php'>Click here to go back to the HomePage</a>";
+    }
+}else{
+
+    echo "Invalid email!";
+
 }
-
-
-
