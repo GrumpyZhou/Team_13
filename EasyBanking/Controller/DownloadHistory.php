54a1baa1c950d5d58ee1be1f2d61b073fc9e139c
Jonas Sticha <jonas.sticha@tum.de>
2015-12-04 17:07:48 +0100

    DownloadHistory.php: Prevent privilege escalation

--- EasyBanking/Controller/DownloadHistory.php
+++ EasyBanking/Controller/DownloadHistory.php
@@ -2,16 +2,29 @@
 session_start();
 require_once('../Model/TransactionHistory.php');
 
-if(isset($_POST['iban'])) {
-    $id = $_POST['iban'];
-} else {
-    $id = $_SESSION['iban'];
+if(isset($_SESSION['isEmployee'])) {
+	$id = "";
+	if($_SESSION['isEmployee'] == false) {
+		$id = $_SESSION['iban'];
+	} else {
+		if(isset($_POST['iban'])) {
+			$id = $_POST['iban'];
+		} else {
+			exit;
+		}
+	}
+	$outputPathAbs = "/tmp/TransactionHistory_" . $id . ".pdf";
+
+	TransactionHistory::ExportPDF($id, $outputPathAbs);
+	if(file_exists($outputPathAbs)) {
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="'.basename($outputPathAbs).'"');
+		header('Expires: 0');
+		header('Cache-Control: private, max-age=0, no-cache');
+		header('Content-Length: '.filesize($outputPathAbs));
+		readfile($outputPathAbs);
+		exit;
+	}
 }
-$outputPathAbs = "/Upload/TransactionHistory_" . $id . ".pdf";
-$outputPathRel = ".." . $outputPathAbs;
-$outputPathAbs = $_SERVER['DOCUMENT_ROOT'] . $outputPathAbs;
-
-TransactionHistory::ExportPDF($id, $outputPathAbs);
-
-header("Location:" . $outputPathRel);
 ?>
