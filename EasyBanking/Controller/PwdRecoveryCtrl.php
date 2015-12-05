84cfadf628fd7dfa3e467c8cc96901632390a060
Qunjie Zhou <jennyzqj00@gmail.com>
2015-12-05 09:25:27 +0100

    implement pwd recovery

--- EasyBanking/Controller/PwdRecoveryCtrl.php
+++ EasyBanking/Controller/PwdRecoveryCtrl.php
@@ -1,16 +1,29 @@
 <?php
 session_start();
-require_once('../Model/Account.php');
+require_once('../Model/PWDSecHandler.php');
+
 
 if (isset($_POST['email'])) {
     $email = $_POST['email'];
+    $urlprefix="http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    echo 'current url: '.$urlprefix;
+    if (PWDSecHandler::handlePWDRecovery($email,$urlprefix)) {
 
-    //check whether email exists if so do
-    //generate token and update db
-    //send token via email
-    //check token and update pwd
-    //else
-    //do nothing ! no error ms in case the user account enumeration
+       echo 'Failed to handle the password recovery';
+    }else{
+        echo 'Email has been successfully sent!';
+    }
 }
 
+//  uri :  /id/token
+$uri=basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+if(isset($uri)){
+    $param=explode("/",$uri);
+    if(PWDSecHandler::authenticateToken($param[0],$param[1])){
+        $_SESSION['resetpwd_user']=$param[0];
+        header("Location:../View/pwdreset.php");
+    }else{
+        header("Location:../View/administration.php");
+    }
+}
 
--- EasyBanking/Model/PWDSecHandler.php
+++ EasyBanking/Model/PWDSecHandler.php
@@ -18,27 +18,58 @@ class PWDSecHandler
     {
     }
 
-    //$id = user id
-    private function sendToken($id, $email)
+
+    public static function  handlePWDRecovery($email, $urlprefix)
+    {
+
+        //check whether the user email address exists
+        $dbHandler = DatabaseHandler::getInstance();
+        $query = "SELECT id FROM users WHERE mail_address='" . $email . "';";
+        $res = $dbHandler->execQuery($query);
+        $row = $res->fetch_assoc();
+        if ($row == NULL) {
+            echo "Wrong User email address.";
+            return false;
+        } else {
+            $userID = $row['id'];
+            //send the token to the email address
+            return self::sendTokenURL($userID, $email, $urlprefix);
+        }
+
+    }
+
+    /**
+     * @param $id
+     * @param $email
+     * @param $urlprefix
+     * @return bool
+     */
+    private function sendTokenURL($id, $email, $urlprefix)
     {
         $dbHandler = DatabaseHandler::getInstance();
-        $token = md5(uniqid($id, true));
+        $token = md5(uniqid(1, true)) . md5(time());
 
         //update the token in DB
         $dbHandler->execQuery("UPDATE " . self::$table . " SET token='" . $token . "', valid_until=DATE_ADD(now(), INTERVAL 5 MINUTE)  WHERE user_id='" . $id . "';");
-        $mailText = "Hello,\nPlease use this token to change your password\n\n" . $token . "\nIt is valid within 5mins";
-        mail($email, "Token", $mailText, "From: EasyBanking");
+        $mailText = "Hello,\nPlease click the URL to change your password\n\n" . $urlprefix . "/" . id . "/" . $token . "\nIt is valid within 5mins";
+        return mail($email, "Token", $mailText, "From: EasyBanking");
     }
+
 
     //check whether the user has the valid token
-    public function authenticateToken($id, $token)
+    public static function authenticateToken($id, $token)
     {
-        $expected = DatabaseHandler::getInstance()->execQuery("SELECT token FROM " . self::$table . " WHERE id='" . $id . "' and valid_until-now()>0 ;");
-        return $token == $expected ? true : false;
+        $dbHandler = DatabaseHandler::getInstance();
+        $query = "SELECT token FROM " . self::$table . " WHERE id='" . $id . "' and valid_until-now()>0 ;";
+        $res = $dbHandler->execQuery($query);
+        $row = $res->fetch_assoc();
+        $expected = $row['token'];
+        return $token == $expected;
+
     }
 
 
-    public function resetPwd($id, $passwd)
+    public static function resetPwd($id, $passwd)
     {
         $dbHandler = DatabaseHandler::getInstance();
         $hasedpwd = hash("sha256", $passwd, FALSE);
@@ -46,24 +77,30 @@ class PWDSecHandler
 
     }
 
+
+
+
+
+
+
     //lock the user after 3 failed login attempt for 10mins
-    public function lock($id)
+    public static function lock($id)
     {
         DatabaseHandler::getInstance()->execQuery("UPDATE users SET locked_until=DATE_ADD(now(), INTERVAL 10 MINUTE)  WHERE failed_login_attempt>3 and user_id='" . $id . "';");
     }
 
-    public function unlock($id)
+    public static function unlock($id)
     {
         DatabaseHandler::getInstance()->execQuery("UPDATE users SET failed_login_attempt=0 WHERE user_id='" . $id . "';");
     }
 
-    public function incFailedAtmp($id)
+    public static function incFailedAtmp($id)
     {
         DatabaseHandler::getInstance()->execQuery("UPDATE users SET failed_login_attempt=failed_login_attempt+1 WHERE user_id='" . $id . "';");
     }
 
     //check whether the user has been locked
-    public function isLocked($id)
+    public static function isLocked($id)
     {
         $time = DatabaseHandler::getInstance()->execQuery("SELECT locked_until FROM " . self::$table . " WHERE failed_login_attempt>3 and id='" . $id . "' ;");
         return $time - time() > 0 ? true : false;
