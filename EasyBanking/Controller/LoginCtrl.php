a161dc2fdea3e6dbf2efe48093a0aeb709b94198
Qunjie Zhou <jennyzqj00@gmail.com>
2015-12-07 03:06:54 +0100

    Impl login lock out function

--- EasyBanking/Controller/LoginCtrl.php
+++ EasyBanking/Controller/LoginCtrl.php
@@ -1,31 +1,47 @@
 <?php
 session_start();
 require_once('../Model/Account.php');
+require_once('../Model/PWDSecHandler.php');
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $user =Account::login($email, $password);  
-        if(is_string($user)){
-			echo "Error message: ".$user;
-			
-			}
-        else{
-            $_SESSION['email'] = $user->email;
-            $_SESSION['firstname'] = $user->firstName;
-            $_SESSION['lastname'] = $user->lastName;
-       
-            if ($user instanceof Customer) {
-                $_SESSION['isEmployee'] = false;
-                $_SESSION['iban'] = $user->IBAN;
-                $_SESSION['balance'] = $user->balance;   
-                header("Location:../View/account.php");
+    //check whether the user should be lock.
+    $isLocked = PWDSecHandler::isLocked($email);
+
+    if ($isLocked == NULL) {
+        echo 'Invalid email address';
+        echo "<a href='../View/index.php'>Click here to go back to the HomePage</a>";
+    } else {
+        if (is_string($isLocked)) {
+            echo 'You have been locked until' . $isLocked . 'because of three failed attempt login.';
+            echo "<a href='../View/index.php'>Click here to go back to the HomePage</a>";
+            header("Location:../View/index.php");
+        } else {
+            $user = Account::login($email, $password);
+            if (is_string($user)) {
+                echo "Error message: " . $user;
+                PWDSecHandler::incFailedAtmp($email);
             } else {
-                $_SESSION['isEmployee'] = true;
-                header("Location:../View/administration.php");
+
+                PWDSecHandler::clearLock($email);
+                $_SESSION['email'] = $user->email;
+                $_SESSION['firstname'] = $user->firstName;
+                $_SESSION['lastname'] = $user->lastName;
+
+                if ($user instanceof Customer) {
+                    $_SESSION['isEmployee'] = false;
+                    $_SESSION['iban'] = $user->IBAN;
+                    $_SESSION['balance'] = $user->balance;
+                    header("Location:../View/account.php");
+                } else {
+                    $_SESSION['isEmployee'] = true;
+                    header("Location:../View/administration.php");
+                }
             }
         }
     }
+}
 
 
