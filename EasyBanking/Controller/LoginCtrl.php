2bf5a7e2cf9d611a9bdf9e4a2f539b531b3ac80d
Qunjie Zhou <jennyzqj00@gmail.com>
2015-12-07 10:41:13 +0100

    comment changes in LoginCtrl

--- EasyBanking/Controller/LoginCtrl.php
+++ EasyBanking/Controller/LoginCtrl.php
@@ -7,25 +7,25 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    //check whether the user should be lock.
-    $isLocked = PWDSecHandler::isLocked($email);
-
-    if ($isLocked == NULL) {
-        echo 'Invalid email address';
-        echo "<a href='../View/index.php'>Click here to go back to the HomePage</a>";
-    } else {
-        if (is_string($isLocked)) {
-            echo 'You have been locked until' . $isLocked . 'because of three failed attempt login.';
-            echo "<a href='../View/index.php'>Click here to go back to the HomePage</a>";
-            header("Location:../View/index.php");
-        } else {
+//    //check whether the user should be lock.
+//    $isLocked = PWDSecHandler::isLocked($email);
+//
+//    if ($isLocked == NULL) {
+//        echo 'Invalid email address';
+//        echo "<a href='../View/index.php'>Click here to go back to the HomePage</a>";
+//    } else {
+//        if (is_string($isLocked)) {
+//            echo 'You have been locked until' . $isLocked . 'because of three failed attempt login.';
+//            echo "<a href='../View/index.php'>Click here to go back to the HomePage</a>";
+//            header("Location:../View/index.php");
+//        } else {
             $user = Account::login($email, $password);
-            if (is_string($user)) {
-                echo "Error message: " . $user;
-                PWDSecHandler::incFailedAtmp($email);
-            } else {
-
-                PWDSecHandler::clearLock($email);
+//            if (is_string($user)) {
+//                echo "Error message: " . $user;
+//                PWDSecHandler::incFailedAtmp($email);
+//            } else {
+//
+//                PWDSecHandler::clearLock($email);
                 $_SESSION['email'] = $user->email;
                 $_SESSION['firstname'] = $user->firstName;
                 $_SESSION['lastname'] = $user->lastName;
@@ -39,9 +39,9 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
                     $_SESSION['isEmployee'] = true;
                     header("Location:../View/administration.php");
                 }
-            }
-        }
-    }
+//            }
+//        }
+//    }
 }
 
 
