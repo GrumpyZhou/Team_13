20ae9f300e7d1553d78dd721edec58bd101cbf28
Qunjie Zhou <jennyzqj00@gmail.com>
2016-01-27 13:52:23 +0100

    modify session fix

--- EasyBanking/Controller/LoginCtrl.php
+++ EasyBanking/Controller/LoginCtrl.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+session_regenerate_id(TRUE);
 require_once('../Model/Account.php');
 require_once('../Model/PWDSecHandler.php');
 
--- EasyBanking/Controller/LogoutCtrl.php
+++ EasyBanking/Controller/LogoutCtrl.php
@@ -2,8 +2,6 @@
 session_start();
 session_destroy();
 session_unset();
-session_start();
-session_regenerate_id(TRUE); 
 
 header("Location:../View/index.php");
 exit();
